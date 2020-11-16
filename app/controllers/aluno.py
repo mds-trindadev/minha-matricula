ec64a9567817f33d7f09709d088c2f180727f6a8
Nilo Mendonca <nilojrmendonca@gmail.com>
2020-11-16 09:06:56 -0300

    Criacao das classes Departamento, Disciplina, Turma, Curso e Aluno e implementacao dos metodos mais simples

---
+++ app/controllers/aluno.py
@@ -0,0 +1,38 @@
+class Aluno:
+	""" Classe Aluno """
+
+	turmasCursadas = []
+	gradeHoraria = []
+
+	def __init__(self, curso, turmasCursadas=None, gradeHoraria=None):
+		"""Construtor"""
+
+		self.curso = curso
+		self.turmasCursadas.append(turmasCursadas)
+		self.gradeHoraria.append(gradeHoraria)
+
+	def adicionarTurma(self, turma):
+		"""Adiciona uma turma a lista de turmas do aluno"""
+
+		self.turmasCursadas.append(turma)
+
+	def removerTurma(self, indice):
+		"""Remove uma turma da lista com turmas do aluno"""
+
+		del self.turmasCursadas[indice]
+
+	def consultarTurmas(self):
+		"""Retorna uma lista com as turmas cursadas pelo aluno"""
+
+		return self.turmasCursadas
+
+	def consultarGradeHoraria(self):
+		"""Retorna uma lista com a grade horaria do aluno"""
+
+		return self.gradeHoraria
+
+	def sugerirGradeHoraria(self):
+		"""Algoritmo para sugestao de grade horaria"""
+
+	def extrairDadosPDF(self):
+		"""Algoritmo para extracao dos dados do arquivo PDF"""
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -5,19 +5,29 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+from app.controllers.turma import Turma
+from app.controllers.turma import Disciplina
+from app.controllers.aluno import Aluno
+from app.controllers.semestre import Semestre
+from app.controllers.curso import Curso
+
 cred = credentials.Certificate(r"./app/controllers/serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://minha-matricula.firebaseio.com/'
 })
 
-# LISTA DE DEPARTAMENTOS JA REVISADOS:
-# ADM, BOT, CCA, CDS, CDT, CEL, CEM, CEN, CET, CFS, CIC, CID, COM, COT, DAN, DAP, DEG, DEX, DIN, DSC, FGA
+# LISTA DE DEPARTAMENTOS:
+# ADM, BOT, CCA, CDS, CDT, CEL, CEM, CEN, CET, CFS, CIC, CID, COM, COT, DAN, DAP, DEG, DEX, DIN, DSC, ECL, ECO, 
+# EFL, ELA, ENC, ENE, ENF, ENM, EPR, EST, FAC, FAR, FAU, FAV, FCE, FCI, FCS, FDD, FEA, FED, FEF, FGA, FIL, FIT, 
+# FMD, FTD, FUP, GEA, GEM, GPP, HIS, ICB, ICH, IDA, IFD, IGD, ILD, IPD, IQD, IRI, JOR, LET, MAT, MUS, NUT, ODT, 
+# PAD, PCL, PRO, SER, SOL, TAU, TEC, TEL, VIS, ZOO
+
 nomeDepartamento = 'FGA'
 
 @app.route("/")
 def home():
 	# Referencia a no do banco de dados
-	ref = db.reference('/'+nomeDepartamento)
+	ref = db.reference('/disciplina/' + nomeDepartamento)
 	cont = 0
 
 	# Lista que armazena dados
@@ -33,26 +43,30 @@ def home():
 		if cont > 3:
 			break
 
+		turma = Turma()
 		# adiciona o codigo da disciplina na lista
-		data.append('Codigo: ' + codigo)
+		turma.codigo = codigo
 		info = ref.child(codigo).get()
 		
 		# adiciona o nome e a carga horaria da disciplina na lista, por esta presente em todas as disciplinas nao e feita a verificacao da existencia dos dados
-		data.append(info['nome'])
-		data.append(info['cargaHoraria'])
+		turma.nome = info['nome']
+		turma.cargaHoraria = info['cargaHoraria']
 
 		# verifica se a disciplina tem ementa disponivel e a adiciona na lista
 		if 'ementa' in info:
-			data.append(info['ementa'])
+			turma.ementa = info['ementa']
 
 		# percorre as turmas disponiveis na disciplina
 		if 'turmas' in info:
 			for i in info['turmas']:
 				# adiciona as informacoes referentes a turma na lista
-				data.append(info['turmas'][i]['periodo'])
-				data.append(info['turmas'][i]['professor'])
+				turma.sigla = info['turmas'][i]
+				turma.periodo = info['turmas'][i]['periodo']
+				turma.professor = info['turmas'][i]['professor']
 				if 'horario' in info['turmas'][i]:
-					data.append(info['turmas'][i]['horario'])
+					turma.horario = info['turmas'][i]['horario']
+
+				data.append(turma)
 
 	# chama a View e passa a lista "data" como parametro
 	return render_template('home.html', data=data)
