055a92b23b06d099bc42c7f3f602341c2cc53596
Irwin Schmitt <irwinschmitt@gmail.com>
2020-11-19 12:11:13 -0300

    Frontend initial setup

---
+++ frontend/src/components/SearchPage.vue
@@ -0,0 +1,16 @@
+<template>
+<section id="searchpage">
+    <SearchBar/>
+</section>
+
+</template>
+
+<script>
+import SearchBar from "./searchPage/SearchBar"
+export default {
+    name: "SearchPage",
+    components: {
+        SearchBar: SearchBar,
+    }
+};
+</script>
