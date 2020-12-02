5b3413786f020659dde5ba7dd42550aad1234fd0
Irwin Schmitt <irwinschmitt@gmail.com>
2020-12-02 17:07:55 -0300

    Clear saved courses

--- frontend/src/components/AppToolbar.vue
+++ frontend/src/components/AppToolbar.vue
@@ -3,14 +3,33 @@
     <v-app-bar app flat outlined>
       <v-spacer />
       <v-btn icon @click="toggleDrawer">
+        <v-badge
+          v-if="savedQty > 0"
+          color="orange"
+          :content="savedQty"
+          left
+        ></v-badge>
         <v-icon>mdi-format-list-bulleted-square</v-icon>
       </v-btn>
     </v-app-bar>
   </section>
 </template>
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "AppToolbar",
+  data: () => ({
+    savedQty: 0,
+  }),
+  computed: {
+    ...mapState(["courses"]),
+  },
+  watch: {
+    "courses.saved"() {
+      this.savedQty = this.courses.saved.length;
+    },
+  },
   methods: {
     toggleDrawer() {
       this.$emit("toggle-drawer");
