23db3e9cab71059ae1d6ad840aa46def2e475f0c
Irwin Schmitt <irwinschmitt@gmail.com>
2020-12-02 03:04:37 -0300

    Open dialog when click on filter button

--- frontend/src/components/SearchToolbar.vue
+++ frontend/src/components/SearchToolbar.vue
@@ -12,13 +12,41 @@
 
       <v-col>
         <v-slide-group show-arrows>
-          <v-slide-item v-for="filter of filters" :key="filter.label">
-            <v-btn class="mx-2" outlined rounded small>
+          <v-slide-item v-for="(filter, key) of filters" :key="filter.label">
+            <v-btn class="mx-2" outlined rounded small @click="openFilter(key)">
               {{ filter.label }}
             </v-btn>
           </v-slide-item>
         </v-slide-group>
       </v-col>
+    </v-row>
+    <v-row v-if="dialog" justify="center">
+      <v-dialog v-model="dialog" scrollable max-width="500px">
+        <v-card>
+          <v-card-title>{{ filters[dialogKey].label }}</v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>
+            <v-checkbox
+              v-for="(option, key) of filters[dialogKey].options"
+              :key="key"
+              v-model="selected[dialogKey]"
+              :value="option"
+              :label="option.toString()"
+              hideDetails
+            >
+            </v-checkbox>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Fechar
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Salvar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-row>
   </section>
 </template>
@@ -30,9 +58,11 @@ export default {
   name: "SearchToolbar",
   data: () => ({
     columns: 3,
-    selections: {
-      campus: [],
-      department: [],
+    dialog: false,
+    dialogKey: null,
+    selected: {
+      campi: [],
+      departments: [],
       credits: [],
     },
   }),
@@ -47,6 +77,10 @@ export default {
   methods: {
     getFilters() {
       this.$store.dispatch("getFilters");
+    },
+    openFilter(key) {
+      this.dialogKey = key;
+      this.dialog = true;
     },
   },
 };
