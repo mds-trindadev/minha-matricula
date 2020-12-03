b4a735a6dcc533a4af6032b3a6e5b209aeae93c0
Nilo Mendonca <nilojrmendonca@gmail.com>
2020-12-03 13:02:27 -0300

    Correções na classe Turma e na requisição de disciplinas

--- backend/app/controllers/turma.py
+++ backend/app/controllers/turma.py
@@ -1,5 +1,6 @@
 import re
 
+vetorTurma = []
 class Departamento:
 	""" Classe Departamento """
 
@@ -16,7 +17,7 @@ class Departamento:
 class Disciplina(Departamento):
 	""" Classe Disciplina """
 
-	def __init__(self, codigo, nome, ementa, preRequisitos, cargaHoraria):
+	def __init__(self, codigo=None, nome=None, ementa=None, preRequisitos=None, cargaHoraria=None, departamento=None, campus=None):
 		"""Inicializador"""
 
 		Departamento.__init__(self, codigo)
@@ -27,12 +28,29 @@ class Disciplina(Departamento):
 		self.creditos = 0
 		self.prioridade = 0
 		self.concluida = False
+		self.campus = campus
+		self.departamento = departamento
 
 	def getDisciplina(self):
 		"""Retorna um dicionario com as informacoes referentes a disciplina"""
 
-		self.preRequisitos = re.findall(r'[A-Z]{3}[0-9]{4}', self.preRequisitos)
+		# self.preRequisitos = re.findall(r'[A-Z]{3}[0-9]{4}', self.preRequisitos)
 		self.creditos = int(re.findall(r'\d+', str(self.cargaHoraria))[0])/15
+
+		if self.departamento == 'FGA':
+			self.campus = 'FGA'
+		elif self.departamento == 'FCE':
+			self.campus = 'FCE'
+		elif self.departamento == 'FUP':
+			self.campus = 'FUP'
+		else:
+			self.campus = 'Darcy Ribeiro'
+
+		dictTurmas = {}
+		n = 0;
+		for i in vetorTurma:
+			dictTurmas[n] = i
+			n += 1
 
 		return {
 				'codigo': self.codigo,
@@ -42,36 +60,34 @@ class Disciplina(Departamento):
 				'cargaHoraria': self.cargaHoraria,
 				'creditos': self.creditos,
 				'prioridade': self.prioridade,
-				'concluida': self.concluida
+				'concluida': self.concluida,
+				'campus': self.campus,
+				'departamento': self.departamento,
+				'turmas': dictTurmas
 				}
+
+	def addTurma(self, turma):
+		"""Adiciona novas turmas"""
+
+		vetorTurma.append(turma)
 
 class Turma(Disciplina):
 	""" Classe Turma """
 
-	def __init__(self, codigo=None, nome=None, ementa=None, preRequisitos=None, cargaHoraria=None, sigla=None, horario=None, professor=None, periodo=None):
+	def __init__(self, turma=None, horario=None, professor=None, periodo=None):
 		"""Inicializador"""
 
-		Disciplina.__init__(self, codigo, nome, ementa, preRequisitos, cargaHoraria)
-		self.sigla = sigla
+		self.turma = turma
 		self.horario = horario
 		self.professor = professor
+		self.periodo = periodo
 
 	def getTurma(self):
 		"""Retorna um dicionario com as informacoes referentes a turma"""
 
-		self.preRequisitos = re.findall(r'[A-Z]{3}[0-9]{4}', self.preRequisitos)
-		self.creditos = int(re.findall(r'\d+', str(self.cargaHoraria))[0])/15
 		return {
-				'codigo': self.codigo,
-				'nome': self.nome, 
-				'ementa': self.ementa, 
-				'preRequisitos': self.preRequisitos, 
-				'cargaHoraria': self.cargaHoraria,
-				'creditos': self.creditos,
-				'sigla': self.sigla,
+				'turma': self.turma,
 				'horario': self.horario,
 				'professor': self.professor,
-				'periodo': self.periodo,
-				'prioridade': self.prioridade,
-				'concluida': self.concluida
+				'periodo': self.periodo
 				}
