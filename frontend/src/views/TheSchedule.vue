bd629a3feb38baca90aeed43c430659812625c24
Irwin Schmitt <irwinschmitt@gmail.com>
2020-12-03 10:48:05 -0300

    Set up schedule page

--- frontend/src/views/TheSchedule.vue
+++ frontend/src/views/TheSchedule.vue
@@ -1,18 +1,16 @@
 <template>
   <section id="schedule">
-    <AppToolbar :title="title" />
+    <h1>Schedule</h1>
+    <h1>Schedule</h1>
+    <h1>Schedule</h1>
     <h1>Schedule</h1>
   </section>
 </template>
 
 <script>
-import AppToolbar from "@/components/AppToolbar";
-
 export default {
   name: "TheSchedule",
-  components: {
-    AppToolbar,
-  },
+
   data: () => ({
     title: "Grade Horária",
   }),
